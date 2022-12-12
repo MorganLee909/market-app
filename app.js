ccd65a3b808efd07fe22946b74cad150cb054b9a
Lee Morgan <me@leemorgan.io>
2022-12-12 16:41:22 -0500

    Create server file for application.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const compression = require("compression");
+
+const app = express();
+app.use(express.static(`${__dirname}/dist`));
+
+app.get("/", (req, res)=>{
+    return res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+module.exports = app;
